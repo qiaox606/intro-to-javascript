40b2eeb016c2378d2a3c08275264db50fcdb0cdc
qiaox606 <78217834+qiaox606@users.noreply.github.com>
2021-03-05 22:33:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+var name="Isabella";
+console.log(name);
+
+var number = 1;
+console.log(number);
+
+const mynumber = 1;
+console.log(mynumber.toString);
+
+console.log(null || 1);
+
+console.log('I','S','A','B','L','L','A');
+
+console.log(2+3, 'hi', true);
+
+var firstname = 'Isabella';
+var lastname = 'QIAO';
+console.log(firstname,lastname);
+
+var intergers = 50 + 50; 
+console.log(intergers);
+
+var yourname = "Isabella";
+
